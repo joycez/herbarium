014fa501c245cb469bd025acc121122a2888faf5
Joyce Zhang <joycezhang@ag-biopl-w80123nw5pj.ag.cornell.edu>
2013-10-22 15:53:52 -0400

    homepage done

---
+++ config/environment.rb
@@ -0,0 +1,16 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Herbarium::Application.initialize!
+
+
+VIRTUAL_HERBARIUM = 'Virtual Herbarium'
+TCF = 'Tompkins County Flora'
+CUPAC = 'CUPAC'
+PLANT_SYSTEMATICS = 'Plant Systematics'
+
+LINK_TO_VH = '#'
+LINK_TO_TCF = 'http://tcf.bh.cornell.edu'
+LINK_TO_CUPAC = 'http://cupac.bh.cornell.edu/'
+LINK_TO_PS = 'http://www.plantsystematics.org/'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Herbarium::Application.config.secret_key_base = '29778475ae4741f2db1894718e8588e4ecb05e94c7c78038c22944b3882eb8b7a70286650d164059ea7abcae1adf898e6085be8eaf9161ca12dbff168452b902'
